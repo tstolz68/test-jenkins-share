ed66d9d72085609e9e826cc5ce040033abe3bb9f
tstolz <tstolz@ccctechcenter.org>
2020-03-27 14:14:36 -0700

    Moved groovy file into var

---
+++ var/log4jLogger.groovy
@@ -0,0 +1,14 @@
+import org.apache.logging.log4j.Logger
+import org.apache.logging.log4j.LogManager
+
+def logInfo(String logMessage) {
+
+    logger.info(logMessage)
+
+}
+
+def logWarn(String logMessage) {
+
+    logger.warn(logMessage)
+
+}
