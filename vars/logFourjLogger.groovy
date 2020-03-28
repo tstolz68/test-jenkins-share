e3e2c47e56dc7a0bfffcbd12e5313c44535a0338
tstolz <tstolz@ccctechcenter.org>
2020-03-28 14:52:19 -0700

    Testing logmessage

--- vars/logFourjLogger.groovy
+++ vars/logFourjLogger.groovy
@@ -1,15 +1,17 @@
-import org.apache.logging.log4j.Logger
-import org.apache.logging.log4j.LogManager
+//import org.apache.logging.log4j.Logger
+//import org.apache.logging.log4j.LogManager
 import groovy.transform.Field
 
 def logInfo(String logMessage) {
 
-    logger.info(logMessage)
+    //logger.info(logMessage)
+    echo "INFO - " + logMessage
 
 }
 
 def logWarn(String logMessage) {
 
-    logger.warn(logMessage)
+    //logger.warn(logMessage)
+    echo "WARN - " + logMessage
 
 }
