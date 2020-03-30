d3d53b34782a657aa58dbc6021632283e1dc21fb
tstolz <tstolz@ccctechcenter.org>
2020-03-30 11:46:58 -0700

    Added Grab to code #2

--- vars/logFourjLogger.groovy
+++ vars/logFourjLogger.groovy
@@ -1,6 +1,6 @@
-@Grapes(
+//
     @Grab(group='org.apache.logging.log4j', module='log4j-api', version='2.0.2')
-)
+//)
 import org.apache.logging.log4j.Logger
 import org.apache.logging.log4j.LogManager
 import groovy.transform.Field
