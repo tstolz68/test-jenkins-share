28e85c42896a6f91b29617ef67f6c1c6783c183f
tstolz <tstolz@ccctechcenter.org>
2020-03-30 11:25:12 -0700

    Testing with Grape

--- vars/logFourjLogger.groovy
+++ vars/logFourjLogger.groovy
@@ -1,17 +1,15 @@
-//import org.apache.logging.log4j.Logger
-//import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.apache.logging.log4j.LogManager
 import groovy.transform.Field
 
 def logInfo(String logMessage) {
 
-    //logger.info(logMessage)
-    echo "INFO - " + logMessage
+    logger.info(logMessage)
 
 }
 
 def logWarn(String logMessage) {
 
-    //logger.warn(logMessage)
-    echo "WARN - " + logMessage
+    logger.warn(logMessage)
 
 }
