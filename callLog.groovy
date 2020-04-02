163a4fa9ea67adfb9bf660942b67995af5d84963
tstolz <tstolz@ccctechcenter.org>
2020-04-02 11:46:32 -0700

    Updates to modules

--- callLog.groovy
+++
@@ -1,8 +0,0 @@
-Class Log4J = ((GroovyClassLoader) this.class.classLoader).parseClass new File("Log4JLogger.groovy")
-
-logger = Log4J.newInstance()
-
-logger.logInfoLevel("This is an INFO message") 
-logger.logDebugLevel("This is an DEBUG message") 
-logger.logWarnLevel("This is an WARN message") 
-logger.logErrorLevel("This is an ERROR message") 
