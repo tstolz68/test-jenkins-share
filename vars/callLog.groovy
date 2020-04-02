a19d7864e77d30238824ceb3490943d569d5acdc
tstolz <tstolz@ccctechcenter.org>
2020-04-02 11:20:33 -0700

    Added vars dir with modules

---
+++ vars/callLog.groovy
@@ -0,0 +1,19 @@
+#!groovy
+//TODO: Put this in the header for shared library files
+Class Log4J = ((GroovyClassLoader) this.class.classLoader).parseClass new File("Log4JLogger.groovy")
+logger = Log4J.newInstance()
+//***
+
+def call(Map args) {
+    testOne()
+}
+
+def testOne() {
+    logger.logInfoLevel("This is an INFO message") 
+}
+
+//Test output here
+//logger.logInfoLevel("This is an INFO message") 
+//logger.logDebugLevel("This is an DEBUG message") 
+//logger.logWarnLevel("This is an WARN message") 
+//logger.logErrorLevel("This is an ERROR message") 
