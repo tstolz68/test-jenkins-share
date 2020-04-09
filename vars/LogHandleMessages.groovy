e0466d7575a0cc90b012ed5d0b62ff98123950b4
tstolz <tstolz@ccctechcenter.org>
2020-04-09 11:51:52 -0700

    Made more changes

--- vars/LogHandleMessages.groovy
+++ vars/LogHandleMessages.groovy
@@ -2,28 +2,41 @@
 
 def loadColors() {
     NC='\033[0m'
-    RED='\033[0,31m'
-    GREEN='\033[0,32m'
-    BLUE='\033[0,34m'
-    CYAN='\033[0,36m'
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    BLUE='\033[0;34m'
+    CYAN='\033[0;36m'
 }
 
 def info(String message, String logLevels) {
-    loadColors()
-    sh """set +x; echo -e "${NC}[INFO] - $message ${NC}" """
+    if (logLevels.contains('INFO')) {
+        loadColors()
+        println(NC + "[INFO] - $message" + NC) 
+    }
 }
 
 def debug(String message, String logLevels) {
-    loadColors()
-    sh """set +x; echo -e "${GREEN}[INFO] - $message ${NC}" """
+    if (logLevels.contains('DEBUG')) {
+       loadColors()
+       println(GREEN + "[DEBUG] - $message" + NC)
+    }
 }
 
 def warn(String message, String logLevels) {
-    loadColors()
-    sh """set +x; echo -e "${CYAN}[INFO] - $message ${NC}" """
+    if (logLevels.contains('WARN')) {
+       loadColors()
+       println(CYAN + "[WARN] - $message" + NC)
+    }
 }
 
 def error(String message, String logLevels) {
-    loadColors()
-    sh """set +x; echo -e "${RED}[INFO] - $message ${NC}" """
+    if (logLevels.contains('ERROR')) {
+       loadColors()
+       println(RED + "[ERROR] - $message" + NC)
+    }
 }
+
+this.info("This is an info message", "INFO,DEBUG,WARN,ERROR")
+this.debug("This is an debug message", "INFO,DEBUG,WARN,ERROR")
+this.warn("This is an warning message", "INFO,DEBUG,WARN,ERROR")
+this.error("This is an error message", "INFO,DEBUG,WARN,ERROR")
--- vars/callLog.groovy
+++ vars/callLog.groovy
@@ -1,42 +1,14 @@
 #!groovy
 import groovy.transform.Field
-/*
-//***** Import Log Processor to handle log requests ****
-def script = new GroovyScriptEngine( '.' ).with {
-  loadScriptByName( 'Log4JLogger.groovy' )
-} 
-this.metaClass.mixin script
-//*****
-
-def testOne() {
-    println("HERE WE ARE!!")
-    logInfoLevel("This is an INFO message") 
-}
-
-//def NewLogger = new Log4JLogger()
-
-//Test output here
-logInfoLevel("This is an INFO message") 
-//logger.logDebugLevel("This is an DEBUG message") 
-//logger.logWarnLevel("This is an WARN message") 
-//logger.logErrorLevel("This is an ERROR message") 
-*/
-//import Log4JLogger.groovy
 
 @Field logLevels = "INFO,ERROR" //params.env_loglevel
-//evaluate(new File("./Log4JLogger.groovy"))
 
-//@BaseScript Log4JLogger 
+LogHandler = new LogHandleMessages()
 
 def testOne() {
-   
-    //println("HERE WE ARE!! - parameter value is: " + logLevels)
-    //def file = new File('Log4JLogger.groovy') 
-    //println "File? ${file.isFile()}" 
-    //evaluate(new File("./Log4JLogger.groovy"))
 
-    LogHandleMessages.info("This is an INFO message", logLevels)
-    LogHandleMessages.error("This is an ERROR message", logLevels)
+    LogHandler.info("This is an INFO message", logLevels)
+    LogHandler.error("This is an ERROR message", logLevels)
 
 }
 
