8a2dae4295c54a2943532a6a2b0c28ebe337b3a9
Thomas Stolz <tomsmagiclaptop@Thomass-MacBook-Pro.local>
2020-04-10 10:37:04 -0700

    Changed Foo to call()

--- vars/Foo.groovy
+++ vars/Foo.groovy
@@ -1,8 +1,17 @@
-b = new Bar()
+//b = new Bar()
 
+/*
 def test() {
     b.logInfoMessage("This is an INFO message")
     b.logErrorMessage("This is an ERROR message")
 }
+*/
 
-test()
+def call(Map config) {
+    b = new Bar()
+
+    b.logInfoMessage("This is an INFO message")
+    b.logErrorMessage("This is an ERROR message")
+}
+
+//test()
